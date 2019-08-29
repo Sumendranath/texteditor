8c7c4825ee4b1b8d239e668b6a48ab3b0029ea94
Sumen <sumendran@gmail.com>
2019-08-29 05:01:12 +0000

    Press 'q' to exit from the program

--- kilo.c
+++ kilo.c
@@ -1,6 +1,7 @@
 #include<unistd.h>
 int main() {
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    /*After entering the string, add 'q' at the end of the line to quit */
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
